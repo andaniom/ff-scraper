4aa4291af229d9805f868a5ca7b350bfd410f742
martin <martin_andanio@tokoonderdil.id>
2022-12-13 15:13:59 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='WebScrapingNews',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='andaniom',
+    author_email='andaniom1@gmail.com',
+    description='andaniom'
+)
