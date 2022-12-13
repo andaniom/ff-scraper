76cfc0ea02eed433590107ef10b4b90081b4f0d2
martin <martin_andanio@tokoonderdil.id>
2022-12-13 17:04:00 +0700

    first

--- api/index.py
+++ api/index.py
@@ -9,5 +9,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain')
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
-        Scraper().main()
+        # Scraper().main()
         return
